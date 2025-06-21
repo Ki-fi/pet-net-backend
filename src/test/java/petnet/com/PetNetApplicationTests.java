89e2ec3f9e4e829422730c92273f7486e51e4c23
Ki-fi <k.meekels@gmail.com>
2025-06-21 11:17:56 +0200

    Project opzet

---
+++ src/test/java/petnet/com/PetNetApplicationTests.java
@@ -0,0 +1,13 @@
+package petnet.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
