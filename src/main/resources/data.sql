0b7aaf36ec4ae8a77fb590cbbb0259561bd352b7
Ki-fi <k.meekels@gmail.com>
2025-07-11 16:17:00 +0200

    mock data aangepast

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO users(user_id, first_name, preposition, last_name)
 VALUES (1, 'Tom', '', 'Kats'),
-       (2, 'Merel', 'van', 'Dieren'),
+       (2, 'Kitty', 'van', 'Dieren'),
        (3, 'Leo', 'den', 'Purr');
 
 INSERT INTO accounts(account_id, email, password, user_id)
@@ -11,6 +11,6 @@ VALUES (1, 'tom@email.com', 'password', 1),
 
 INSERT INTO posts (post_id, creator, post_status, start_date, end_date, title, remark, created_at)
 VALUES
-    (1, 1, 'ACTIVE', '2025-01-05', '2025-01-10', 'Looking for weekend cat care', 'Mittens loves tuna and hates the vacuum cleaner.', CURRENT_TIMESTAMP),
-    (2, 2, 'ACTIVE', '2025-02-14', '2025-02-20', 'Need sitter while on ski trip', 'Snuggles at night, hides during the day.', CURRENT_TIMESTAMP),
-    (3, 3, 'ACTIVE', '2025-03-01', '2025-03-08', 'Cat feeding and playtime needed', 'Watch out, he opens cupboard doors.', CURRENT_TIMESTAMP);
+    (1, 1, 'ACTIVE', '2025-01-05', '2025-01-10', 'Zoekt weekend katten oppas', 'Mittens houdt van tonijn maar haat de stofzuiger.', CURRENT_TIMESTAMP),
+    (2, 2, 'ACTIVE', '2025-02-14', '2025-02-20', 'Oppas gezocht voor tijdens ski trip', 'Verstopt zich overdag, maar knuffelt s nachts.', CURRENT_TIMESTAMP),
+    (3, 3, 'ACTIVE', '2025-03-01', '2025-03-08', 'Kat voer- en speeltijd gevraagd', 'Opgepast! Hij opent keukenkastjes', CURRENT_TIMESTAMP);
