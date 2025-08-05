591df59f8b31ba8ee66c55dd82b846144b0eae52
Ki-fi <k.meekels@gmail.com>
2025-08-05 11:10:31 +0200

    secret key en db wachtwoord naar .env verplaatst

--- src/main/java/petnet/com/security/JwtService.java
+++ src/main/java/petnet/com/security/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -16,10 +17,14 @@ import java.util.function.Function;
 
 @Service
 public class JwtService {
-    private final static String SECRET_KEY = "bWVvd01peDlMaXZlc1doaXNrZXJUd2lzdDQyIVB1cnJNYWNoaW5lUG91bmNlUGF3cw==";
+    private final String secretKey;
+
+    public JwtService(@Value("${app.security.secret-key}") String secretKey) {
+        this.secretKey = secretKey;
+    }
 
     private Key getSigningKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
     public String extractUsername(String token) {
