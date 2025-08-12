f1962bec867fad7f4afd6c39a006f1c3cedd8ccb
Ki-fi <k.meekels@gmail.com>
2025-08-12 14:21:31 +0200

    Integration tests

--- src/test/java/petnet/com/services/PostManagerUnitTest.java
+++ src/test/java/petnet/com/services/PostManagerUnitTest.java
@@ -29,7 +29,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class PostManagerTest {
+class PostManagerUnitTest {
 
     @Mock
     PostRepository postRepository;
--- src/test/java/petnet/com/services/PostServiceManagerUnitTest.java
+++ src/test/java/petnet/com/services/PostServiceManagerUnitTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class PostServiceManagerTest {
+class PostServiceManagerUnitTest {
 
     PostServiceManager manager = new PostServiceManager();
 
